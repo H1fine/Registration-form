18f769244082663a8d9208f29046d12879cdd6d6
H1fine <111656813+H1fine@users.noreply.github.com>
2025-10-05 15:59:08 -0500

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,15 @@
+const form = document.getElementById('contactform');
+// const outputDiv = document.getElementById('output');
+
+form.addEventListener("click", function(event){
+  event.preventDefault();
+ 
+
+    // const submitButton = document.getElementById("submitButton").value;
+
+    
+   //alert('Form submission prevented!'); 
+
+    // I am not sure of what to do next?Please Help...
+});
+
